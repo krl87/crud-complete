c186855733f5386db6002b69aa8ffa3930515c91
krl87 <kayley.luftig@gmail.com>
2016-03-08 09:40:05 -0500

    initial commit

---
+++ config/db.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Kayley on 2016-03-08.
+ */
+//create pulic connection string
+
+module.exports = {
+    //local
+    //'url': 'mongodb://localhost/test',
+
+    'url': 'mongodb://kayley:password@ds064278.mlab.com:64278/thisisadatabase'
+};
---
+++ public/javascripts/scripts.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Kayley on 2016-03-08.
+ */
+
+//delete confirmation
+$('.confirmation').on('click', function(){
+    return confirm('Are you sure you want to delete this?');
+});
